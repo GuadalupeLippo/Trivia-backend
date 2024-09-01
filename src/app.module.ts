013f9fdff5f8e07e8142c22566b5b7bd0f94454e
Agustina Ortolachipi <agusortolachipi10@gmail.com>
2024-09-01 18:30:20 -0300

    Creación de la entidad Categoria y sus recursos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RespuestaModule } from './respuesta/respuesta.module';
+import { CategoriaModule } from './categoria/categoria.module';
 
 @Module({
-  imports: [],
+  imports: [RespuestaModule, CategoriaModule],
   controllers: [AppController],
   providers: [AppService],
 })
