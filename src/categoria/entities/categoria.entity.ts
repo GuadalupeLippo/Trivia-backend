6ff2b732e726f63a1ea7282a86acd090ac552f2d
Agustina Ortolachipi <agusortolachipi10@gmail.com>
2024-09-11 18:50:07 -0300

    Entidad categoria

--- src/categoria/entities/categoria.entity.ts
+++ src/categoria/entities/categoria.entity.ts
@@ -1,1 +1,13 @@
-export class Categoria {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Categoria {
+  @PrimaryGeneratedColumn()
+  idCategoria: number;
+
+  @Column()
+  tipo: string;
+
+  @Column({ nullable: true }) 
+  logo: string;
+}
