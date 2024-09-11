f0b097abb7d0e2e8a291b965792c594cb5d27d27
Agustina Ortolachipi <agusortolachipi10@gmail.com>
2024-09-11 18:28:39 -0300

    Entidad respuesta

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RespuestaModule } from './respuesta/respuesta.module';
 
 @Module({
-  imports: [],
+  imports: [RespuestaModule],
   controllers: [AppController],
   providers: [AppService],
 })
