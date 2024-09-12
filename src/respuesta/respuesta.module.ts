e0827c73fbf0beb4ca1232643e886e870e8ca324
Agustina Ortolachipi <agusortolachipi10@gmail.com>
2024-09-12 19:35:56 -0300

    quite el paquete typeormmodule

--- src/respuesta/respuesta.module.ts
+++ src/respuesta/respuesta.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Respuesta } from './entities/respuesta.entity';
-import { Question } from '../questions/entities/question.entity';
 import { RespuestaService } from './respuesta.service';
 import { RespuestaController } from './respuesta.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Respuesta, Question])],
+  imports: [],
   controllers: [RespuestaController],
   providers: [RespuestaService],
 })
