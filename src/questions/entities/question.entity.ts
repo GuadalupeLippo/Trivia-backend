69e4ea6c9b8e1f6f8c2bad45bd79ac71ebe73e20
Guadalupe Lippo <gualippo@gmail.com>
2024-09-12 21:36:48 -0300

    creacion de rama para partidas

--- src/questions/entities/question.entity.ts
+++ src/questions/entities/question.entity.ts
@@ -2,38 +2,7 @@ import { TipoDificult } from "src/tipo-dificult/entities/tipo-dificult.entity";
 import { Respuesta } from "src/respuesta/entities/respuesta.entity";
 import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
-@Entity()
-export class Question {
-  @PrimaryGeneratedColumn()
-  id: number;
 
-  @Column()
-  description: string;
-
-  @Column()
-  category: string;
-
-  @ManyToOne(() => TipoDificult, (tipoDificult) => tipoDificult.questions, { eager: true })
-  tipodificult: TipoDificult;
-
-  @OneToMany(() => Respuesta, (respuesta) => respuesta.question)
-  respuestas: Respuesta[];
-}
-
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  description: string;
-
-  @Column()
-  category: string;
-
-  @ManyToOne(() => TipoDificult, (tipoDificult) => tipoDificult.questions, { eager: true })
-  tipodificult: TipoDificult;
-import { Respuesta } from "src/respuesta/entities/respuesta.entity";
-import { TipoDificult } from "src/tipo-dificult/entities/tipo-dificult.entity";
-import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Question {
@@ -43,14 +12,14 @@ export class Question {
   @Column()
   description: string;
 
+
+ //esta seria una relacion muchas a una
   @Column()
   category: string;
 
   @ManyToOne(() => TipoDificult, (tipoDificult) => tipoDificult.questions, { eager: true })
   tipodificult: TipoDificult;
 
-  @OneToMany(() => Respuesta, (respuesta) => respuesta.question)
-  respuestas: Respuesta[];
-}
 
 }
+
