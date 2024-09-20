8a70d224b25f6a2f26ea9a96bdfa318619675a8b
Guadalupe Lippo <gualippo@gmail.com>
2024-09-19 22:20:33 -0300

    creacion de dtos de games

--- src/games/dto/create-game.dto.ts
+++ src/games/dto/create-game.dto.ts
@@ -1,1 +1,23 @@
-export class CreateGameDto {}
+import { IsInt } from "class-validator";
+
+export class CreateGameDto {
+    @IsInt()
+    playerId : number;
+
+    @IsInt()
+    score: number;
+
+    @IsInt()
+    categoryId : number;
+
+    @IsInt()
+    questionId : number;
+
+    @IsInt()
+    answerId : number;
+
+    @IsInt()
+    dificultly : number;
+
+}
+
