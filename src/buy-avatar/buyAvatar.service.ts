e3ec4d3646cba452abc0a086939e8afcbe1dd7b5
AilenMassa <ailenmassa1@gmail.com>
2024-09-26 21:04:58 -0300

    metodos crud buyAvatar, se agrego la entidad player

--- src/buy-avatar/buyAvatar.service.ts
+++ src/buy-avatar/buyAvatar.service.ts
@@ -3,6 +3,7 @@ import { CreateBuyAvatarDto } from './dto/create-buyAvatar.dto';
 import { UpdateBuyAvatarDto } from './dto/update-buyAvatar.dto';
 import { BuyAvatar } from './entities/buyAvatar.entity';
 import { Avatar } from 'src/avatars/entities/avatar.entity';
+import { Player } from 'src/player/entities/player.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
@@ -11,32 +12,44 @@ export class BuyAvatarService {
     @Inject('AVATAR_REPOSITORY')
     private avatarRepository: Repository<Avatar>,
     @Inject('BUYAVATAR_REPOSITORY')
-    private buyAvatarRepository: Repository<BuyAvatar>
+    private buyAvatarRepository: Repository<BuyAvatar>,
+    @Inject('PLAYER_REPOSITORY') 
+    private playerRepository: Repository<Player>
   ) {}
 
   async findAll(): Promise<BuyAvatar[]> {
-    const buyAvatar = await this.buyAvatarRepository.find({ relations: ["purchasedAvatar"] });
+    const buyAvatar = await this.buyAvatarRepository.find({ relations: ["purchasedAvatar","player"] });
     if (!buyAvatar.length) throw new NotFoundException("No purchases in database")
     return buyAvatar
 
 }
 
 async createOne(createBuyAvatarDto: CreateBuyAvatarDto): Promise<BuyAvatar> {
-  const { avatarId } = createBuyAvatarDto;
+  const { avatarId, playerId } = createBuyAvatarDto;
 
   const avatar = await this.avatarRepository.findOne({ where: { id: avatarId } });
   if (!avatar) {
     throw new NotFoundException(`Avatar with ID ${avatarId} not found`);
   }
 
+  const player = await this.playerRepository.findOne({ where: { id: playerId } });
+    if (!player) {
+      throw new NotFoundException(`Player with ID ${playerId} not found`);
+    }
+
   const buyAvatar = new BuyAvatar();
   buyAvatar.purchasedAvatar = avatar;
+  buyAvatar.player = player;  
   return await this.buyAvatarRepository.save(buyAvatar);
 }
 
 async updateOne(id: number, updateBuyAvatarDto: UpdateBuyAvatarDto): Promise<BuyAvatar> {
-  const { avatarId } = updateBuyAvatarDto;
-  const buyAvatar = await this.buyAvatarRepository.findOne({ where: { id }, relations: ["purchasedAvatar"] });
+  const { avatarId, playerId } = updateBuyAvatarDto;
+
+  const buyAvatar = await this.buyAvatarRepository.findOne({
+    where: { id },
+    relations: ["purchasedAvatar", "player"],  
+  });
   if (!buyAvatar) {
     throw new NotFoundException(`BuyAvatar with ID ${id} not found`);
   }
@@ -48,6 +61,15 @@ async updateOne(id: number, updateBuyAvatarDto: UpdateBuyAvatarDto): Promise<Buy
     }
     buyAvatar.purchasedAvatar = avatar;
   }
+
+  if (playerId) {
+    const player = await this.playerRepository.findOne({ where: { id: playerId } });
+    if (!player) {
+      throw new NotFoundException(`Player with ID ${playerId} not found`);
+    }
+    buyAvatar.player = player;
+  }
+
   return await this.buyAvatarRepository.save(buyAvatar);
 }
 
