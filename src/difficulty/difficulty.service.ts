d5a99551f3340469465158a1368bbf0712c3872a
Yessica <yessilpd91@outlook.com>
2024-10-08 20:00:58 -0300

    comenzando clase difficulty

--- src/difficulty/difficulty.service.ts
+++ src/difficulty/difficulty.service.ts
@@ -1,19 +1,25 @@
-import { Injectable, NotAcceptableException } from '@nestjs/common';
+import { Inject, Injectable, NotAcceptableException } from '@nestjs/common';
 import { CreateDifficultyDto } from './dto/create-tipo-dificult.dto';
 import { UpdateDifficultyDto } from './dto/update-tipo-dificult.dto';
 import { Question } from 'src/questions/entities/question.entity';
+import { Difficulty } from './entities/difficulty.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class DifficultyService {
-  questionRepository: any;
-  create(createTipoDificultDto: UpdateDifficultyDto) {
-    return 'This action adds a new tipoDificult';
+  constructor(@Inject('DIFFICULTY_REPOSITORY')
+  private questionRepository: Repository<Difficulty>,
+){}
+
+  async create(createTipoDificultDto: UpdateDifficultyDto) {
+    const nivel = this.questionRepository.create(createTipoDificultDto)
+    return this.questionRepository.save(nivel)
   }
 
-  async findAll(): Promise<Question[]> {
-    const question = await this.questionRepository.find()
-    if(!question) throw new NotAcceptableException("No question in BasedeDatos")
-      return question;
+  async findAll(): Promise<Difficulty[]> {
+    const difficulty = await this.questionRepository.find()
+    if(!difficulty) throw new NotAcceptableException("No difficulty in Database")
+      return difficulty;
   }
 
   findOne(id: number) {
