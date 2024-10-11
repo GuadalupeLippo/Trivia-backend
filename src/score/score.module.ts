a62165be0ab042b515e8ea1faa19b5e04b7ed951
Guadalupe Lippo <gualippo@gmail.com>
2024-10-10 21:44:25 -0300

    metodos crud listos

--- src/score/score.module.ts
+++ src/score/score.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ScoreService } from './score.service';
 import { ScoreController } from './score.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { scoreProviders } from './score.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ScoreController],
-  providers: [ScoreService],
+  providers: [ScoreService,
+    ...scoreProviders
+  ],
 })
 export class ScoreModule {}
--- src/score/score.service.ts
+++ src/score/score.service.ts
@@ -1,26 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateScoreDto } from './dto/create-score.dto';
 import { UpdateScoreDto } from './dto/update-score.dto';
+import { Repository } from 'typeorm';
+import { Score } from './entities/score.entity';
+import { scoreRepository } from 'src/constants/constant';
 
 @Injectable()
 export class ScoreService {
-  create(createScoreDto: CreateScoreDto) {
-    return 'This action adds a new score';
-  }
+  constructor(
+    @Inject(scoreRepository)
+    private scoreRepository: Repository<Score>,
+  ){}
 
-  findAll() {
-    return `This action returns all score`;
+  async createOneScore(createScoreDto: CreateScoreDto): Promise<Score> {
+    const score = this.scoreRepository.create(createScoreDto)
+    return await this.scoreRepository.save(score)
+  }
+  async findAllScore(): Promise<Score[]> {
+    const score= await this.scoreRepository.find()
+    if (!score) throw new NotFoundException("No scores in database")
+    return score
   }
 
   findOne(id: number) {
     return `This action returns a #${id} score`;
   }
 
-  update(id: number, updateScoreDto: UpdateScoreDto) {
-    return `This action updates a #${id} score`;
+  async updateScore(id: number, updateScoreDto: UpdateScoreDto) {
+    const score = await this.scoreRepository.preload({
+      id: id,
+      ...updateScoreDto
+    })
+    if (!score) throw new NotFoundException(`score with id ${id} not found`)
+    return await this.scoreRepository.save(score)
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} score`;
-  }
+  async deleteScore(id: number) : Promise <String> {
+    const score = await this.scoreRepository.findOne({ where: { id } });
+    if (!score) throw new NotFoundException(`Score with id ${id} not found`);
+    await this.scoreRepository.remove(score)
+    return 'score Deleted'
+  } 
 }
