e37c74097cd6387e6b72f163beb8b55276be2945
AilenMassa <ailenmassa1@gmail.com>
2024-11-14 12:37:01 -0300

    cambios mercado pago

--- src/buy-score/buy-score.service.ts
+++ src/buy-score/buy-score.service.ts
@@ -1,20 +1,143 @@
-import { Injectable, Inject, NotFoundException} from '@nestjs/common';
+// import { Injectable, Inject, NotFoundException} from '@nestjs/common';
+// import { CreateBuyScoreDto } from './dto/create-buy-score.dto';
+// import { UpdateBuyScoreDto } from './dto/update-buy-score.dto';
+// import { BuyScore } from './entities/buy-score.entity';
+// import { Player } from 'src/player/entities/player.entity';
+// import { Score } from 'src/score/entities/score.entity';
+// import { Repository } from 'typeorm';
+// import { buyScoreRepository, playerRepository, scoreRepository} from 'src/constants/constant';
+
+// import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+
+// const client = new MercadoPagoConfig(
+//   { accessToken: process.env.MP_ACCESS_TOKEN }
+// );
+
+
+
+// @Injectable()
+// export class BuyScoreService {
+//   constructor(
+//     @Inject(buyScoreRepository)
+//     private buyScoreRepository: Repository<BuyScore>,
+//     @Inject(scoreRepository)
+//     private scoreRepository: Repository<Score>,
+//     @Inject(playerRepository) 
+//     private playerRepository: Repository<Player>
+//   ) {}
+
+//   async createBuyScore(createBuyScoreDto: CreateBuyScoreDto): Promise<BuyScore> {
+//     try {
+//       const player = await this.playerRepository.findOne({ where: { id: createBuyScoreDto.playerId } });
+//       if (!player) {
+//         throw new NotFoundException('Player not found');
+//       }
+
+//       const score = await this.scoreRepository.findOne({ where: { id: createBuyScoreDto.scoreId } });
+//       if (!score) {
+//         throw new NotFoundException('Score not found');
+//       }
+
+//       const preference = new Preference();
+//       preference.items = [
+//         {
+//           title: `Compra de ${score.points}`, 
+//           quantity: 1,
+//           unit_price: score.price,
+//           currency_id: 'ARS', 
+//         },
+//       ];
+
+//       // ir a la URL de pago
+//       const preferenceResponse = await client.preference.create(preference);
+
+//       const newBuyScore = this.buyScoreRepository.create({
+//         score,
+//         player,
+//       });
+
+//       await this.buyScoreRepository.save(newBuyScore);
+
+//       return newBuyScore;
+//     } catch (err) {
+//       console.error(err.message);
+//     }
+//   }
+
+  
+ 
+//     async findAll(): Promise<BuyScore[]> {
+//       const buyScore = await this.buyScoreRepository.find({ relations: ["score","player"] });
+//       if (!buyScore.length) throw new NotFoundException("No score purchased in database")
+//       return buyScore
+  
+//   }
+  
+//   async findBuyScoreById(buyScoreId: number): Promise<BuyScore> {
+//     try {
+//     const buyScore = await this.buyScoreRepository.findOne({ where: { id: buyScoreId  },
+//       relations:['score',
+//         'player'
+//       ] });
+//     if (!buyScore) throw new NotFoundException("No purchase of point in database");
+//       return buyScore
+//     } catch {
+//         throw new NotFoundException("No purchase of point in database")}
+//   }
+
+//   async updateOne(id: number, UpdateBuyScoreDto: UpdateBuyScoreDto): Promise<BuyScore> {
+//     const { scoreId, playerId } = UpdateBuyScoreDto;
+  
+//     const buyScore = await this.buyScoreRepository.findOne({
+//       where: { id },
+//       relations: ["score", "player"],  
+//     });
+//     if (!buyScore) {
+//       throw new NotFoundException(`BuyScore with ID ${id} not found`);
+//     }
+  
+//     if (scoreId) {
+//       const score = await this.scoreRepository.findOne({ where: { id: scoreId } });
+//       if (!score) {
+//         throw new NotFoundException(`Score with ID ${scoreId} not found`);
+//       }
+//       buyScore.score = score;
+//     }
+  
+//     if (playerId) {
+//       const player = await this.playerRepository.findOne({ where: { id: playerId } });
+//       if (!player) {
+//         throw new NotFoundException(`Player with ID ${playerId} not found`);
+//       }
+//       buyScore.player = player;
+//     }
+  
+//     return await this.buyScoreRepository.save(buyScore);
+//   }
+
+//   async remove(id: number): Promise<void> {
+//     const buyScore = await this.buyScoreRepository.findOne({ where: { id } });
+//     if (!buyScore) {
+//       throw new NotFoundException(`BuyScore with ID ${id} not found`);
+//     }
+//     await this.buyScoreRepository.remove(buyScore);
+//   }
+  
+// }
+  
+import { Injectable, Inject, NotFoundException } from '@nestjs/common';
 import { CreateBuyScoreDto } from './dto/create-buy-score.dto';
 import { UpdateBuyScoreDto } from './dto/update-buy-score.dto';
 import { BuyScore } from './entities/buy-score.entity';
 import { Player } from 'src/player/entities/player.entity';
 import { Score } from 'src/score/entities/score.entity';
 import { Repository } from 'typeorm';
-import { buyScoreRepository, playerRepository, scoreRepository} from 'src/constants/constant';
+import { buyScoreRepository, playerRepository, scoreRepository } from 'src/constants/constant';
 
-import { MercadoPagoConfig, Preference } from 'mercadopago';
+import * as MercadoPago from 'mercadopago'; 
 
-
-const client = new MercadoPagoConfig(
-  { accessToken: process.env.MP_ACCESS_TOKEN }
-);
-
-
+MercadoPago.configurations.setAccessToken(process.env.MP_ACCESS_TOKEN);
 
 @Injectable()
 export class BuyScoreService {
@@ -23,8 +146,8 @@ export class BuyScoreService {
     private buyScoreRepository: Repository<BuyScore>,
     @Inject(scoreRepository)
     private scoreRepository: Repository<Score>,
-    @Inject(playerRepository) 
-    private playerRepository: Repository<Player>
+    @Inject(playerRepository)
+    private playerRepository: Repository<Player>,
   ) {}
 
   async createBuyScore(createBuyScoreDto: CreateBuyScoreDto): Promise<BuyScore> {
@@ -39,18 +162,19 @@ export class BuyScoreService {
         throw new NotFoundException('Score not found');
       }
 
-      const preference = new Preference();
-      preference.items = [
-        {
-          title: `Compra de ${score.points}`, 
-          quantity: 1,
-          unit_price: score.price,
-          currency_id: 'ARS', 
-        },
-      ];
+      const preference = {
+        items: [
+          {
+            title: `Compra de ${score.points}`,
+            quantity: 1,
+            unit_price: score.price,
+            currency_id: 'ARS',
+          },
+        ],
+      };
 
-      // ir a la URL de pago
-      const preferenceResponse = await client.preference.create(preference);
+      // Crea la preferencia en MercadoPago
+      const preferenceResponse = await MercadoPago.preferences.create(preference);
 
       const newBuyScore = this.buyScoreRepository.create({
         score,
@@ -59,44 +183,41 @@ export class BuyScoreService {
 
       await this.buyScoreRepository.save(newBuyScore);
 
+      // Devuelve la respuesta con el link de pago
       return newBuyScore;
+      
     } catch (err) {
-      throw new Error(err.message);
+      console.error(err.message);
+      throw new Error('Error creating buy score');
     }
   }
 
-  
- 
-    async findAll(): Promise<BuyScore[]> {
-      const buyScore = await this.buyScoreRepository.find({ relations: ["score","player"] });
-      if (!buyScore.length) throw new NotFoundException("No score purchased in database")
-      return buyScore
-  
+  async findAll(): Promise<BuyScore[]> {
+    const buyScore = await this.buyScoreRepository.find({ relations: ['score', 'player'] });
+    if (!buyScore.length) throw new NotFoundException('No score purchased in database');
+    return buyScore;
   }
-  
+
   async findBuyScoreById(buyScoreId: number): Promise<BuyScore> {
-    try {
-    const buyScore = await this.buyScoreRepository.findOne({ where: { id: buyScoreId  },
-      relations:['score',
-        'player'
-      ] });
-    if (!buyScore) throw new NotFoundException("No purchase of point in database");
-      return buyScore
-    } catch {
-        throw new NotFoundException("No purchase of point in database")}
+    const buyScore = await this.buyScoreRepository.findOne({
+      where: { id: buyScoreId },
+      relations: ['score', 'player'],
+    });
+    if (!buyScore) throw new NotFoundException('No purchase of point in database');
+    return buyScore;
   }
 
   async updateOne(id: number, UpdateBuyScoreDto: UpdateBuyScoreDto): Promise<BuyScore> {
     const { scoreId, playerId } = UpdateBuyScoreDto;
-  
+
     const buyScore = await this.buyScoreRepository.findOne({
       where: { id },
-      relations: ["score", "player"],  
+      relations: ['score', 'player'],
     });
     if (!buyScore) {
       throw new NotFoundException(`BuyScore with ID ${id} not found`);
     }
-  
+
     if (scoreId) {
       const score = await this.scoreRepository.findOne({ where: { id: scoreId } });
       if (!score) {
@@ -104,7 +225,7 @@ export class BuyScoreService {
       }
       buyScore.score = score;
     }
-  
+
     if (playerId) {
       const player = await this.playerRepository.findOne({ where: { id: playerId } });
       if (!player) {
@@ -112,7 +233,7 @@ export class BuyScoreService {
       }
       buyScore.player = player;
     }
-  
+
     return await this.buyScoreRepository.save(buyScore);
   }
 
@@ -123,6 +244,4 @@ export class BuyScoreService {
     }
     await this.buyScoreRepository.remove(buyScore);
   }
-  
 }
-  
